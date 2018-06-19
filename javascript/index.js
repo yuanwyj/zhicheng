f29dbaa0352207cb7c629c808a5362ec2092587d
yuanwyj <1229102737@qq.com>
2018-06-19 20:56:47 +0800

    引入腾讯地图

--- javascript/index.js
+++ javascript/index.js
@@ -104,18 +104,15 @@ function goTopEx() {
 
 // 初始化地图
 function initMap() {
-        // 百度地图API功能
-    var map = new BMap.Map("map");    // 创建Map实例
-    map.centerAndZoom(new BMap.Point(114.0282567321,22.6604458094), 16);  // 初始化地图,设置中心点坐标和地图级别
-    // 添加地图类型控件
-    map.addControl(new BMap.MapTypeControl({
-        mapTypes:[
-            BMAP_NORMAL_MAP,
-            BMAP_HYBRID_MAP
-        ]}));     
-    map.setCurrentCity("深圳");          // 设置地图显示的城市 此项是必须设置的
-    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-    
-    var marker = new BMap.Marker(new BMap.Point(114.0282567321,22.6604458094)); 
-    map.addOverlay(marker); 
+
+    var center = new qq.maps.LatLng(22.6542300000,114.0218100000);
+    var map = new qq.maps.Map(document.getElementById('map'),{
+        center: center,
+        zoom: 16
+    });
+    //创建marker
+    var marker = new qq.maps.Marker({
+        position: center,
+        map: map
+    });
 }
